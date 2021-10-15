be611597e1582a0008d719c03b5c425e7a5e5063
league <league@imac-109.attlocal.net>
2021-10-15 16:56:51 -0700

    guestbook ip

--- src/_02_array_list_guestbook/GuestBook.java
+++ src/_02_array_list_guestbook/GuestBook.java
@@ -7,34 +7,49 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+
+import _01_array_list_visualizer.data.ArrayList;
+
 //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
 public class GuestBook implements ActionListener {
-	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
-	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
-	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
+	JButton button = new JButton("ADD NAME");
+	JButton clickything = new JButton("VIEW NAMES");
+	ArrayList<String> list = new ArrayList<String>();
+	// Create a GUI with two buttons. One button reads "Add Name" and the other
+	// button reads "View Names".
+	// When the add name button is clicked, display an input dialog that asks the
+	// user to enter a name. Add
+	// that name to an ArrayList. When the "View Names" button is clicked, display a
+	// message dialog that displays
 	// all the names added to the list. Format the list as follows:
 	// Guest #1: Bob Banders
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
-void setup() {
-	JFrame frame= new JFrame("THINGAMABOBBER");
-	JPanel panel=new JPanel();
-	JButton button=new JButton("ADD NAME");
-	JButton clickything=new JButton("VIEW NAMES");
-	frame.add(panel);
-	panel.add(button);
-	panel.add(clickything);
-	frame.setVisible(true);
-	frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
-	frame.pack();
-	button.addActionListener(this);
-}
+	void setup() {
+		JFrame frame = new JFrame("THINGAMABOBBER");
+		JPanel panel = new JPanel();
+		frame.add(panel);
+		panel.add(button);
+		panel.add(clickything);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+		frame.pack();
+		button.addActionListener(this);
+	}
+	//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
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JButton clicked = (JButton) e.getSource();
+		if(clicked==button) {
+			String names = JOptionPane.showInputDialog("Enter a Name!");
+			list.add(names);
+		}
+		if(clicked=clickything) {
+		for (int i = 0; list.size() > i; i++) {
 
-@Override
-public void actionPerformed(ActionEvent e) {
-	e.getSource();
-	// TODO Auto-generated method stub
-	
-}
+		// TODO Auto-generated method stub
+
+	}}}
+
 }
